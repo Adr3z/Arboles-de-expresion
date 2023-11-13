3e131f70791259e9d8ab59dfa4979ac4a5005788
Adreeezz <127537483+Adreeezz@users.noreply.github.com>
2023-11-13 07:29:02 +0000

    Funciones para la pila completas

--- expression_tree.c
+++ expression_tree.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "expression_tree.h"
 
-node_tree_t* create_node( int value )
+node_tree_t *create_node( int value )
 {
-    node_tree_t* new = (node_tree_t*)malloc(sizeof(node_tree_t));
+    node_tree_t *new = (node_tree_t*)malloc(sizeof(node_tree_t));
     new->value = value;
-    new->left = NULL;
-    new->right = NULL;
+    new->left = new->right = NULL;
+    return new;
 }
 
 int ascii(char op)
@@ -25,12 +26,17 @@ int ascii(char op)
             return 47;
             break;
     }
+    return 0;
 }
 
 void initialize(stack_t *s)
 {
-    s->size = 0;
     s->top = NULL;
+}
+
+int isEmpty(stack_t *stack) 
+{
+    return stack->top == NULL;
 }
 
 void push(stack_t *s, node_tree_t *node)
@@ -41,9 +47,9 @@ void push(stack_t *s, node_tree_t *node)
     s->top = new;
 }
 
-node_tree_t* pop(stack_t *s)
+node_tree_t *pop(stack_t *s)
 {
-    if(s->size == 0){
+    if(isEmpty(s)){
         return NULL;
     }
 
@@ -56,12 +62,59 @@ node_tree_t* pop(stack_t *s)
     return node;
 }
 
+void free_tree(node_tree_t *root)
+{
+    if (root != NULL) {
+        free_tree(root->left);
+        free_tree(root->right);
+        free(root);
+    }
+}
+
+void free_stack(stack_t *s)
+{
+    while (!isEmpty(s)) {
+        node_tree_t *tmp = pop(s);
+        free(tmp);
+    }
+}
+
+void free_all(node_tree_t *root, stack_t *s)
+{
+    free_tree(root);
+    free_stack(s);
+}
 
 //Main functions
 node_tree_t *build_tree(char *postfix_expression)
 {
     printf("The expression is %s\n", postfix_expression);
-    return NULL;
+
+    stack_t s;
+    initialize(&s);
+
+    char *current = postfix_expression;
+
+    while( *current != '\0'){
+        char c = *current;
+        if(isdigit(c)){
+            int value = c - '0';
+            node_tree_t *node = create_node(value);
+            push(&s, node);
+        }else if(c == '+' || c == '-' || c == '*' || c == '/') {
+            int value = ascii(c);
+
+            node_tree_t *op_node = create_node(value);
+            op_node->right = pop(&s);
+            op_node->left = pop(&s);
+
+            push(&s, op_node);
+        }
+        current++;
+    }
+    node_tree_t *root = pop(&s);
+    free_all(root, &s);
+    return root;
 }
 
 void print_tree(node_tree_t *root_node)
--- expression_tree.h
+++ expression_tree.h
@@ -12,6 +12,8 @@ Códigos ASCII
 */
 
 typedef struct node_tree node_tree_t;
+typedef struct stack stack_t;
+typedef struct stack_node stack_node_t;
 
 struct node_tree{
     node_tree_t *right;
@@ -19,26 +21,33 @@ struct node_tree{
     int value;
 };
 
-typedef struct stack_node{
+struct stack_node{
     node_tree_t *node;
     stack_node_t *next;
-}stack_node_t;
+};
 
-typedef struct stack{
+struct stack{
     stack_node_t *top;
-    int size;
-}stack_t;
+};
 
 //creates a node
-node_tree_t* create_node( int value );
+node_tree_t *create_node( int value );
 
 //return ascii
 int ascii(char op);
 
+//free tree
+void free_tree(node_tree_t *root);
+
+//free all
+void free_all(node_tree_t *root, stack_t *s);
+
 //stack functions
-void initialize( stack_t *s);
+void initialize( stack_t *s );
+int isEmpty(stack_t *stack);
 void push(stack_t *s, node_tree_t *node);
-node_tree_t* pop(stack_t *s);
+node_tree_t *pop(stack_t *s);
+void free_stack(stack_t *s);
 
 //Main functions
 node_tree_t *build_tree(char *);
